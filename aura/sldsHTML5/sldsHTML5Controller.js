afc09c3842cea13f564146c86bfee6d6b86a6726
David <david@socialandbeyond.com>
2018-05-18 10:16:33 +0200

    Added the functionality for speech recognition.

--- aura/sldsHTML5/sldsHTML5Controller.js
+++ aura/sldsHTML5/sldsHTML5Controller.js
@@ -1,4 +1,7 @@
 ({
+    /**
+     * Simply function that will make a push notification in the window
+     */
 	notify : function(component, event, helper) {
         var params = event.getParam('arguments');
         if(params && window.Notification && Notification.permission !== "denied") {
@@ -12,11 +15,47 @@
         }
 	},
 
+    /**
+     * It is a functionality to transform a string into sound through the speakers
+     */
 	synthesize : function(component, event, helper) {
         sinthesizer = new SpeechSynthesisUtterance;
         sinthesizer.lang = 'es-ES';
         var msg = event.getParam("arguments").msg;
         sinthesizer.text = msg;
         speechSynthesis.speak(sinthesizer);
-	}
+	},
+
+    /**
+     * It's a functionality to transform speech recognized by the microphone into an string.
+     */
+    speechRecognize :function (component, event, helper) {
+        if ('webkitSpeechRecognition' in window){
+            var params = event.getParam('arguments');
+            var speechRecognizer = helper.createSpeechRecognizer(component);
+            speechRecognizer.start();
+            var phrase = '';
+            speechRecognizer.onresult = function(event) {
+                var finalTranscripts = '';
+                var interimTranscripts = '';
+                for (var i = event.resultIndex; i < event.results.length; i++) {
+                    var transcript = event.results[i][0].transcript;
+                    if(event.results[i].isFinal) {
+                        finalTranscripts += transcript;
+                    } else {
+                        interimTranscripts += transcript;
+                    }
+                }
+                phrase = finalTranscripts + ' ' + interimTranscripts;
+            }
+            speechRecognizer.onend = function(event) {
+                if (params.callback) callback(phrase);
+            }
+            speechRecognizer.onerror = function(event) {
+                console.log(event.error);
+            }
+        } else {
+            alert("Not webkitSpeechRecognition service at Window object");
+        }
+    }
 })
--- aura/sldsHTML5/sldsHTML5Helper.js
+++ aura/sldsHTML5/sldsHTML5Helper.js
@@ -1,5 +1,12 @@
 ({
-	helperMethod : function() {
-		
-	}
+    /**
+     * Create a new instance of WebkitSpeechRecognition and set it up
+     */
+    createSpeechRecognizer : function(component) {
+        var speechRecognizer = new webkitSpeechRecognition();
+        speechRecognizer.continuous = false;
+        speechRecognizer.interimResults = true;
+        speechRecognizer.lang = 'es-ES';
+        return speechRecognizer;
+    },
 })
